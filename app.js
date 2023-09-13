5638a48d7f2f52ddaad9a13a9af561d38b9e7679
seaer <jrd.seaer@gmail.com>
2023-09-13 22:57:00 +0300

    fetch gif

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-console.log("Hello World!\n==========\n");
+"use strict";
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIPHY_KEY = "aYUJQCh0aRXvs7L8MZv8wHyoscMe7hAv";
+let feedbackEle = document.querySelector("#feedback");
+let searchInput = document.querySelector("#searchWord");
+let searchBtn = document.querySelector("#submitSearch");
+let gifEle = document.querySelector("#imageContainer > img");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+searchBtn.addEventListener("click", (event) => {
+  fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchInput.value}`)
+    .then((res) => res.json())
+    .then((body) => {
+        gifEle.src = body.data.images.original.url;
+    })
+    .catch((err) => {
+        console.error(err);
+        feedbackEle.textContent = err.message;
+    });
+});
